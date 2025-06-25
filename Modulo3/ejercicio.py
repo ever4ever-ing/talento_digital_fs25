f93c51e7a88267a38252ea899efc8b26d26d785f
Everardo Alvarado <e.alvarado03@ufromail.cl>
2025-06-24 21:32:29 -0400

    24-06

--- Modulo3/ejercicio.py
+++ Modulo3/ejercicio.py
@@ -1,3 +1,18 @@
 """
 Ingresar numero por teclado y mostrar si es par.
 """
+import logging
+# Set up logging configuration
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+numero = int(input("Ingresa un numero: "))
+logging.info("El numero ingresado es: %s", numero)
+operacion = numero % 2
+logging.debug("El resultado de la operacion es: %s", operacion)
+
+if operacion == 0:
+    print("El numero es Par")
+else:
+    print("El número no es par")
