3cdd4942a82b4c5deb0698285091cd56b019dd54
Everardo Alvarado <e.alvarado03@ufromail.cl>
2025-06-25 20:22:22 -0400

    25-06

--- Modulo3/semana1/modulos/ejemplo3/ruta.py
+++ Modulo3/semana1/modulos/ejemplo3/ruta.py
@@ -1,4 +1,7 @@
 import sys
-
+"""
+Este es un ejemplo de cómo imprimir las rutas de búsqueda de módulos en Python.
+"""
+# Imprimir las rutas de búsqueda de módulos
 for ruta in sys.path:
-    print(ruta)
+    print(ruta)# Esto imprimirá las rutas de búsqueda de módulos en Python.
