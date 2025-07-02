cf9c7de9781977e684a4bc6b66f0fc3c923930c8
Everardo Alvarado <e.alvarado03@ufromail.cl>
2025-07-01 20:14:57 -0400

    01-07

---
+++ Modulo3/07-01/listas/02-acceso-listas.py
@@ -0,0 +1,16 @@
+cajonera = ["pantalones", "camisetas", "calcetines"]
+
+print(cajonera[0]) #Accedemos al cajón con índice 0. Imprime: "pantalones"
+
+print(cajonera[1]) #Accedemos al cajón con índice 1. Imprime: "camisetas"
+
+print(cajonera[2]) #Accedemos al cajón con índice 2. Imprime: "calcetines"
+
+camisetas = cajonera[1]
+
+cajonera[1] = "sueters" #Cambiamos el valor del cajón con índice 1
+
+print(cajonera) #Imprime: ['pantalones', 'sueters', 'calcetines']
+
+cajonera.append(camisetas)
+print(cajonera)
