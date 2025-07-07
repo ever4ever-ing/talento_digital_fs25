7fa85e6f190d11c4128783dc460ca8c37bf61b04
Everardo Alvarado <e.alvarado03@ufromail.cl>
2025-07-07 19:53:57 -0400

    07-07

--- Modulo3/02_tipos_de_datos/07-07/condicionales/solucion.py
+++ Modulo3/02_tipos_de_datos/07-07/condicionales/solucion.py
@@ -3,13 +3,12 @@ material = input("Material (cuero/tela/sintético): ").lower()
 precio = float(input("Precio ($): "))
 garantia = int(input("Garantía (años): "))
 
-
-
-"""
-if _________________________:  # Alta calidad (cumple los 3 criterios )
+if material == "cuero" and precio > 100000 and garantia > 1:
+    # Alta calidad (cumple los 3 criterios )
     print(" Alta calidad: Excelentes materiales, durabilidad y confianza.")
-elif _______________________:  # Calidad media (al menos 2 criterios medios/altos )
+elif (material == "tela" and precio >= 50000) or (garantia >= 1): 
+    # Calidad media (al menos 2 criterios medios/altos )
     print(" Calidad media: Al menos dos criterios.")
-else:                         # Baja calidad (no cumple requisitos )
+else:                         
+    # Baja calidad (no cumple requisitos )
     print(" Baja calidad: No cumple con los estándares mínimos.")
-"""
