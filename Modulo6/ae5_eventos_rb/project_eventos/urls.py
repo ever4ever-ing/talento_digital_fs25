32af4b322abe4f15cfb3e497a761b97d4e70ea3d
ever4ever-ing <e.alvarado03@ufromail.cl>
2025-11-06 21:42:27 -0300

    06-11

--- Modulo6/ae5_eventos_rb/project_eventos/urls.py
+++ Modulo6/ae5_eventos_rb/project_eventos/urls.py
@@ -20,6 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_eventos.urls')),
-    path('login/', include('django.contrib.auth.urls')),
-    path('logout/', include('django.contrib.auth.urls')),
 ]
